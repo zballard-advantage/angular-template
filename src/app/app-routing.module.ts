bd1c9a7c0e97a17d12a8085cdef815e8a43c8ac8
zachary.ballard <zachary.ballard@atlasdsr.com>
2023-05-01 01:55:22 -0500

    rename folder for contact edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContactListComponent } from './components/contact-list/contact-list.component';
-import { ContactEditComponent } from './components/edit-contact/contact-edit.component';
+import { ContactEditComponent } from './components/contact-edit/contact-edit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'contacts', pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {HttpClientModule} from '@angular/common/http';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {ContactEditComponent} from './components/edit-contact/contact-edit.component';
+import {ContactEditComponent} from './components/contact-edit/contact-edit.component';
 import {ContactListComponent} from './components/contact-list/contact-list.component';
 import {ContactStateService} from "./services/contact-state.service";
 
